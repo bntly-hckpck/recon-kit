47ed2b50dc37529a4b698402943b1a2ec552e38a
bntly-hckpck <bntly_dev@pm.me>
2026-09-17 16:43:57 +0000

    main.py: add port range validation

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from port_scanner import port_scanning
 from banner_grabber import banner_grabbing
 from report import report_writing
 
+# check if port is within range (1-65535)
+def port_validation(port_number, arg_name):
+
+    if port_number < 1 or port_number > 65535:
+        print(f"error: {arg_name} must be in range (1-65535)")
+        return False
+    else:
+        return True
+
 def main():
 
     # argument parser
@@ -13,6 +22,12 @@ def main():
     parser.add_argument("start_port", type=int, help="insert first port of scanning range (1-65535)")
     parser.add_argument("end_port", type=int, help="insert last port of scanning range (1-65535)")
     args = parser.parse_args()
+
+    # port validation
+    if not port_validation(args.start_port, "first port of scanning range"):
+        exit(1)
+    if not port_validation(args.end_port, "last port of scanning range"):
+        exit(1)
 
     # extract values from args
     target_ip = args.target_ip
